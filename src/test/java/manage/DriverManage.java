ec8957e743c961110d2f1deff17594888a3600fb
Necmettin <necmettin_kzldg@hotmail.com>
2023-11-22 16:42:52 +0100

    some updates

--- src/test/java/manage/DriverManage.java
+++ src/test/java/manage/DriverManage.java
@@ -1,9 +1,13 @@
 package manage;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import utilities.ConfigReader;
@@ -14,7 +18,7 @@ import java.time.Duration;
 
 public class DriverManage {
 
-    static WebDriver driver;
+    public static WebDriver driver;
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
 
@@ -65,5 +69,67 @@ public class DriverManage {
 
 
         return  driver;
+    }
+
+    public static void setDriver(String browser){
+        switch (browser){
+            case "grid_chrome":{
+                ChromeOptions chromeOptions = new ChromeOptions();
+                try {
+                    driver = new RemoteWebDriver(new URL(ConfigReader.getProperty("url")),chromeOptions);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+
+                System.out.println("***** Selenium Grid Chrome *****");
+                break;
+            }
+            case "grid_firefox":{
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                try {
+                    driver = new RemoteWebDriver(new URL(ConfigReader.getProperty("url")),firefoxOptions);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+
+                System.out.println("***** Firefox  WebDriver *****");
+                break;
+            }
+            case "grid_IE":{
+                InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
+                try {
+                    driver = new RemoteWebDriver(new URL(ConfigReader.getProperty("url")),internetExplorerOptions);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+
+                System.out.println("***** IE WebDriver *****");
+                break;
+            }
+            case "grid_edge":{
+                EdgeOptions edgeOptions = new EdgeOptions();
+                try {
+                    driver = new RemoteWebDriver(new URL(ConfigReader.getProperty("url")),edgeOptions);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+
+                System.out.println("***** Edge WebDriver *****");
+                break;
+            }
+            default:{
+                WebDriverManager.chromedriver().setup();
+                System.out.println("***** Chrome WebDriver *****");
+                driver = new ChromeDriver();
+            }
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+
+    public static void closeDriver(){
+        if (driver!=null){
+            driver.close();
+        }
     }
 }
